d47eca9661d091b9153d606f611b80a441f34224
johnny <JiangN1@089818>
2025-01-08 15:19:19 +0800

    [200] 岛屿数量

---
+++ leetcode/editor/cn/[105]从前序与中序遍历序列构造二叉树.ts
@@ -0,0 +1,64 @@
+//给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并
+//返回其根节点。 
+//
+// 
+//
+// 示例 1: 
+// 
+// 
+//输入: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+//输出: [3,9,20,null,null,15,7]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: preorder = [-1], inorder = [-1]
+//输出: [-1]
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= preorder.length <= 3000 
+// inorder.length == preorder.length 
+// -3000 <= preorder[i], inorder[i] <= 3000 
+// preorder 和 inorder 均 无重复 元素 
+// inorder 均出现在 preorder 
+// preorder 保证 为二叉树的前序遍历序列 
+// inorder 保证 为二叉树的中序遍历序列 
+// 
+//
+// Related Topics 树 数组 哈希表 分治 二叉树 👍 2443 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
+    const n = preorder.length
+    if (n == 0) return null
+    const leftSize = inorder.indexOf(preorder[0])
+    const pre1 = preorder.slice(1, leftSize + 1)
+    const pre2 = preorder.slice(leftSize + 1)
+    const in1 = inorder.slice(0, leftSize)
+    const in2 = inorder.slice(leftSize + 1, n)
+    const left = buildTree(pre1, in1)
+    const right = buildTree(pre2, in2)
+    return new TreeNode(preorder[0], left, right)
+}
+//leetcode submit region end(Prohibit modification and deletion)
