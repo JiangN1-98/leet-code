f66900b0af74c343886e92c047785a97279c89ad
johnny <JiangN1@089818>
2025-01-08 09:37:29 +0800

    [101] 对称二叉树

---
+++ leetcode/editor/cn/[101]对称二叉树.ts
@@ -0,0 +1,60 @@
+//给你一个二叉树的根节点 root ， 检查它是否轴对称。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [1,2,2,3,4,4,3]
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [1,2,2,null,3,null,3]
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [1, 1000] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶：你可以运用递归和迭代两种方法解决这个问题吗？ 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 2852 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSymmetric(root: TreeNode | null): boolean {
+    if (root == null) return true;
+    const isSymmtRee: any = (p: any, q: any) => {
+        if (p == null || q == null) {
+            return p == q
+        }
+        return p.val == q.val && isSymmtRee(p.left, q.right) && isSymmtRee(p.right, q.left)
+    }
+    return isSymmtRee(root.left, root.right)
+};
+//leetcode submit region end(Prohibit modification and deletion)
